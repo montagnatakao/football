1940a4a80a8ed19d7a12f1e42a3d8cc69f2dd6b9
montagnatakao <montagnatakao@gmail.com>
2026-03-23 04:05:15 +0900

    Add files via upload

---
+++ data/matches.js
@@ -0,0 +1,9 @@
+// 試合データ
+// 編集方法: 配列に試合オブジェクトを追加・編集してください
+// type: "リーグ戦" / "カップ戦" / "練習試合"
+// home_away: "HOME" / "AWAY"
+// score_home, score_away: 未実施の場合は null
+var MATCHES_DATA = [
+  { id: 1, type: "交流戦", date: "2026-03-25", time: "12:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",   score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
+  { id: 2, type: "交流戦", date: "2026-03-26", time: "11:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "リバプール・フットボール・センター・川崎" },
+];
---
+++ data/social.js
@@ -0,0 +1,12 @@
+// SNSデータ
+// youtube_url: YouTubeの動画URL
+// instagram_url: Instagramプロフィールページ
+// instagram_posts: 埋め込みたいInstagram投稿のURL（配列）
+var SOCIAL_DATA = {
+  youtube_url: "https://youtube.com/shorts/g0BYpMiclw8?si=3eSWWeZhUbKM3Cdl",
+  instagram_url: "https://www.instagram.com/montagna.takao",
+  instagram_posts: [
+    "https://www.instagram.com/p/DV8Jx12E3jM/",
+    "https://www.instagram.com/p/DWDVHhDE31E/"
+  ]
+};
