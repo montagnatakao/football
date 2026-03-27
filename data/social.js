b3dce67d2705186b7cae895dd490a3d36aa630a5
montagnatakao <montagnatakao@gmail.com>
2026-03-27 23:01:48 +0900

    Update social.js

--- data/social.js
+++ data/social.js
@@ -6,7 +6,7 @@ var SOCIAL_DATA = {
   youtube_url: "https://youtube.com/shorts/g0BYpMiclw8?si=3eSWWeZhUbKM3Cdl",
   instagram_url: "https://www.instagram.com/montagna.takao",
   instagram_posts: [
-    "https://www.instagram.com/p/DWRBNRTEwwV/?utm_source=ig_embed&amp;utm_campaign=loading",
+    "https://www.instagram.com/p/DWRBNRTEwwV/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     "https://www.instagram.com/p/DWDVHhDE31E/"
   ]
 };
