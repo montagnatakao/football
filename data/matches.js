07665f5cafd0ceadb2fcea36740efa67fa3358e5
montagnatakao <montagnatakao@gmail.com>
2026-05-16 00:52:32 +0900

    Fix match details for ID 5
    Updated the date and opponent for match ID 5.

--- data/matches.js
+++ data/matches.js
@@ -8,5 +8,5 @@ var MATCHES_DATA = [
   { id: 2, type: "交流戦", date: "2026-03-26", time: "11:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "リバプール・フットボール・センター・川崎" },
   { id: 3, type: "東京・調布ソサイチ大会 F3カテゴリ", date: "2026-05-05", time: "12:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
   { id: 4, type: "東京・調布ソサイチ大会 F2カテゴリ", date: "2026-05-05", time: "15:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
-  { id: 5, type: "東京・調布ソサイチ大会 F3カテゴリ", date: "2026-06-08or6-22", time: "", home_away: "AWAY", team: "モンターニャ高尾", opponent: "未定",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
+  { id: 5, type: "東京・調布ソサイチ大会 F3カテゴリ", date: "未定", time: "", home_away: "AWAY", team: "モンターニャ高尾", opponent: "未定",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
 ];
