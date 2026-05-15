9673522acb9b90a2effdf8191847951dcb2fd07a
montagnatakao <montagnatakao@gmail.com>
2026-05-16 01:08:38 +0900

    Update matches.js

--- data/matches.js
+++ data/matches.js
@@ -4,8 +4,8 @@
 // home_away: "HOME" / "AWAY"
 // score_home, score_away: 未実施の場合は null
 var MATCHES_DATA = [
-  { id: 1, type: "交流戦", date: "2026-03-25", time: "12:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",   score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
-  { id: 2, type: "交流戦", date: "2026-03-26", time: "11:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "リバプール・フットボール・センター・川崎" },
+  { id: , type: "交流戦", date: "2026-03-25", time: "12:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",   score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
+  { id: , type: "交流戦", date: "2026-03-26", time: "11:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "リバプール・フットボール・センター・川崎" },
   { id: 3, type: "東京・調布ソサイチ大会 F3カテゴリ", date: "2026-05-05", time: "12:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
   { id: 4, type: "東京・調布ソサイチ大会 F2カテゴリ", date: "2026-05-05", time: "15:00", home_away: "AWAY", team: "モンターニャ高尾", opponent: "",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
   { id: 5, type: "UNIVA 7’S TOKYO 第1節", date: "未定", time: "", home_away: "AWAY", team: "モンターニャ高尾", opponent: "未定",  score_home: null, score_away: null, venue: "J-SOCIETY FOOTBALL PARK 調布" },
